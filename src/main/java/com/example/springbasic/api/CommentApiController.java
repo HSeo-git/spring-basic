a54be882b1df8f8472d3175969c42487afa2a8e2
HG.Seo <sophieseo3@gmail.com>
2022-03-29 16:56:46 +0900

    feature(comment CRUD):add comment CRUD(using RestAPI)

---
+++ src/main/java/com/example/springbasic/api/CommentApiController.java
@@ -0,0 +1,50 @@
+package com.example.springbasic.api;
+
+import com.example.springbasic.dto.CommentDto;
+import com.example.springbasic.entity.Comment;
+import com.example.springbasic.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CommentApiController {
+    @Autowired
+    private CommentService commentService;
+
+    //check the list of comments
+    @GetMapping("api/articles/{articleId}/comments")
+    public ResponseEntity<List<CommentDto>> comments(@PathVariable Long articleId) {
+        //send to service
+        List<CommentDto> dtos = commentService.comments(articleId);
+        //response
+        return ResponseEntity.status(HttpStatus.OK).body(dtos);
+    }
+    //create
+    @PostMapping("api/articles/{articleId}/comments")
+    public ResponseEntity<CommentDto> create(@PathVariable Long articleId, @RequestBody CommentDto dto) {
+
+        //send to service
+        CommentDto createDto = commentService.create(articleId, dto);
+        //response
+        return ResponseEntity.status(HttpStatus.OK).body(createDto);
+    }
+
+    //update
+    @PatchMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> update(@PathVariable Long id, @RequestBody CommentDto dto) {
+        CommentDto updateDto = commentService.update(id, dto);
+        return ResponseEntity.status(HttpStatus.OK).body(updateDto);
+    }
+
+    //delete
+    @DeleteMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> delete(@PathVariable Long id) {
+        CommentDto deleted = commentService.delete(id);
+        return ResponseEntity.status(HttpStatus.OK).body(deleted);
+    }
+
+}
---
+++ src/main/java/com/example/springbasic/service/CommentService.java
@@ -0,0 +1,78 @@
+package com.example.springbasic.service;
+
+import com.example.springbasic.dto.CommentDto;
+import com.example.springbasic.entity.Article;
+import com.example.springbasic.entity.Comment;
+import com.example.springbasic.repository.ArticleRepository;
+import com.example.springbasic.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    public List<CommentDto> comments(Long articleId) {
+//        //check the list of comments
+//        List<Comment> comments = commentRepository.findByArticleId(articleId);
+//        //change entity -> dto
+//        List<CommentDto> dtos = new ArrayList<CommentDto>();
+//        for (int i = 0; i < comments.size(); i++) {
+//            Comment c = comments.get(i);
+//            CommentDto dto = CommentDto.createCommentDto(c);
+//            dtos.add(dto);
+//        }
+        //response
+        return commentRepository.findByArticleId(articleId)
+                .stream()
+                .map(comment -> CommentDto.createCommentDto(comment))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional //it deals with db, so it needs roll-back when error happens.
+    public CommentDto create(Long articleId, CommentDto dto) {
+        //check the article and make exception
+        //if error happens this phase, it doesn't go to the next steps(to do this use ElseThrow).
+        Article article = articleRepository.findById(articleId).orElseThrow(()->new IllegalArgumentException("Fail to create! No article"));
+
+        //create comment entity
+        Comment comment = Comment.createComment(dto, article);
+
+        //save the entity in DB
+        Comment created = commentRepository.save(comment);
+
+        //response with DTO
+        return CommentDto.createCommentDto(created);
+    }
+
+    @Transactional
+    public CommentDto update(Long id, CommentDto dto) {
+        //check comment and make exception
+        Comment target = commentRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("Fail to update! no comment"));
+        //update comment
+        target.patch(dto);
+        //resisted updated comment to DB
+        Comment updated = commentRepository.save(target);
+        //change the entity into dto and return
+        return CommentDto.createCommentDto(updated);
+    }
+
+    @Transactional
+    public CommentDto delete(Long id) {
+        //check comment and make exception
+        Comment target = commentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Fail to delete no comment"));
+        //delete comment from db
+        commentRepository.delete(target);
+        //response
+        return CommentDto.createCommentDto(target);
+    }
+}
